6fd1754fc33776fbe9e197d034584a6b26302b35
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-06-29 00:48:55 +0700

    Removed hardcoded jQuery import on template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -151,8 +151,6 @@
 <!-- ./wrapper -->
 
 @stack('footer_scripts')
-<!-- jQuery 2.2.3 -->
-<script src="{{ asset('bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="{{ asset('bower_components/AdminLTE/bootstrap/js/bootstrap.min.js') }}"></script>
 <!-- SlimScroll -->
