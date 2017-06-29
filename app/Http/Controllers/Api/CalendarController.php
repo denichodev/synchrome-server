a2583ff566a2f82be753af5cd5524a13e83d9540
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-06-30 02:32:36 +0700

    Added methods for capability to update calendar in Api\CalendarController

--- app/Http/Controllers/Api/CalendarController.php
+++ app/Http/Controllers/Api/CalendarController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use App\Http\Controllers\Controller;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use App\Calendar;
@@ -107,5 +108,111 @@ class CalendarController extends Controller
                 'result' => 'success',
                 'data' => $data
             ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        if (empty($id)) {
+            return response()
+                ->json([
+                    'error' => ['Calendar ID required']
+                ], 400);
+        }
+
+        $validation = Validator::make($request->all(), [
+            'name' => 'required',
+            'updatedEvents' => 'array',
+            'updatedEvents.*.originalId' => 'required',
+            'updatedEvents.*.title' => 'required',
+            'updatedEvents.*.start' => 'required|date',
+            'deletedEvents' => 'array',
+            'deletedEvents.*.originalId' => 'required',
+            'updatedEvents.*.start' => 'required|date',
+            'newEvents' => 'array',
+            'newEvents.*.title' => 'required',
+            'newEvents.*.start' => 'required|date',
+            'status' => 'required|in:draft,published'
+        ]);
+
+        if ($validation->fails()) {
+            return response()
+                ->json([
+                    'error' => $validation->errors()
+                ], 400);
+        }
+
+        $calendar = Calendar::find($id);
+
+        if (is_null($calendar)) {
+            return response()
+                ->json([
+                    'error' => ['Calendar with ID ' . $id. ' not found']
+                ], 404);
+        }
+
+        try {
+            $userData = $request->all();
+            DB::beginTransaction();
+            $this->batchUpdate(collect($userData['updatedEvents']));
+            $this->batchDelete(collect($userData['deletedEvents']));
+            $this->batchInsert($calendar, collect($userData['newEvents']));
+            DB::commit();
+
+            return response()
+                ->json([
+                    'result' => 'success',
+                    'data' => $calendar
+                ]);
+        } catch (\Exception $e) {
+            DB::rollback();
+
+            return response()
+                ->json([
+                    'error' => [$e->getMessage()]
+                ]);
+        }
+    }
+
+    public function batchInsert($calendar, Collection $events)
+    {
+        $newEvents = [];
+
+        $events->each(function ($newEvent) use (&$newEvents) {
+            $numOfDay = date('N', strtotime($newEvent['start']));
+            array_push($newEvents, new Event([
+                'title' => $newEvent['title'],
+                'start' => $newEvent['start'],
+                'end' => ! empty($newEvent['end']) ? $newEvent['end'] : null,
+                'isWeekday' => empty($newEvent['end']) && ($numOfDay == 6 || $numOfDay == 7) ? true : false,
+                'numOfDay' => $numOfDay
+            ]));
+        });
+
+        $calendar->events()->saveMany($newEvents);
+    }
+
+    public function batchUpdate(Collection $events)
+    {
+        $events->each(function ($updatedEvent) {
+            $numOfDay = date('N', strtotime($updatedEvent['start']));
+            $event = Event::find($updatedEvent['originalId']);
+
+            $event->update([
+                'title' => $updatedEvent['title'],
+                'start' => $updatedEvent['start'],
+                'end' => empty($updatedEvent['end']) || $updatedEvent['end'] == $updatedEvent['start'] ? null : $updatedEvent['end'],
+                'isWeekday' => empty($updatedEvent['end']) && ($numOfDay == 6 || $numOfDay == 7) ? true : false,
+                'numOfDay' => $numOfDay
+            ]);
+        });
+    }
+
+    public function batchDelete(Collection $events)
+    {
+        $deletedEventIds = $events->map(function ($event) {
+            return $event['originalId'];
+        });
+
+        Event::whereIn('id', $deletedEventIds)->delete();
     }
 }
