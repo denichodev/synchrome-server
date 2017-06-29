8478da4232e5d5b1c66082df3baa820de17c02af
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-06-30 01:14:31 +0700

    Added calendars.edit.blade.php

---
+++ resources/views/calendars/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('template')
+@section('title', 'Calendars')
+@section('description', 'Edit Calendar: ' . $calendar->name)
+@section('breadcrumb')
+<li><a href="{{ route('dashboard') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+<li><a href="{{ route('calendars.index') }}"><i class="fa fa-calendar"></i> Calendars</a></li>
+<li class="active"><a href="{{ route('calendars.edit', $calendar->id) }}"><i class="fa fa-edit"></i> Edit Calendar: {{ $calendar->name }}</a></li>
+@endsection
+
+@section('content')
+<div id="cal">
+  <calendar state="edit" id="{{ $calendar->id }}"></calendar>
+</div>
+@endsection
+
+@push('head_scripts')
+<meta name="jwt-token" content="{{ $token }}">
+@endpush
+
+@push('footer_scripts')
+<script src="{{ asset('js/calendar.js') }}"></script>
+@endpush
