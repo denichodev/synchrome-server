7d8e07d56060aeaa4bcd3eb90252673c790ee465
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-07-10 00:59:25 +0700

    Added basic seeder for EventCategory

---
+++ database/seeds/EventCategoriesTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\EventCategory;
+
+class EventCategoriesTableSeeder extends Seeder
+{
+    private $categories = [
+        [
+            'id' => 1,
+            'name' => 'Workday',
+            'color' => '#3c8dbc'
+        ],
+        [
+            'id' => 2,
+            'name' => 'Holiday',
+            'color' => '#e74c3c'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Special Workday',
+            'color' => '#9b59b6'
+        ],
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->categories as $category) {
+            EventCategory::create($category);
+        }
+    }
+}
