1273c183e28a6fe78e9c0817d8ec7b70c6e25ab4
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-08-26 03:18:01 +0700

    Clean-ups on views

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,23 +5,4 @@
 @endsection
 
 @section('content')
-<!-- Default box -->
-<div class="box">
-  <div class="box-header with-border">
-    <h3 class="box-title">@yield('title')</h3>
-  </div>
-  <div class="box-body">
-    Start creating your amazing application!
-  </div>
-  <!-- /.box-body -->
-  <div class="box-footer">
-    Footer
-  </div>
-  <!-- /.box-footer-->
-</div>
-<!-- /.box -->
 @endsection
-
-@push('footer_scripts')
-<script src="{{ asset('bower_components/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
-@endpush
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -24,7 +24,7 @@
 <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
   <header class="app-header navbar">
     <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button">☰</button>
-    <a class="navbar-brand" href="#"></a>
+    <a href="#"></a>
     <ul class="nav navbar-nav d-md-down-none">
       <li class="nav-item"><a class="nav-link navbar-toggler sidebar-toggler" href="#">☰</a></li>
     </ul>
