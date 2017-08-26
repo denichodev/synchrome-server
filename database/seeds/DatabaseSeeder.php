41b3ffb4ecd3c629a06aaf8647271b2f24b33cd4
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-08-27 03:02:16 +0700

    Call mandatory seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(EventCategoriesTableSeeder::class);
+        $this->call(AgencyTypesTableSeeder::class);
+        $this->call(AgenciesTableSeeder::class);
+        $this->call(EchelonsTableSeeder::class);
     }
 }
