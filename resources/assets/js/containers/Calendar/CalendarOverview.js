94ab47c67a442cbc608f496f1907abbc59925a7b
Deni Cho <dencho.12@gmail.com>
2017-08-27 02:25:52 +0700

    Updated CalendarOverview

--- resources/assets/js/containers/Calendar/CalendarOverview.js
+++ resources/assets/js/containers/Calendar/CalendarOverview.js
@@ -4,9 +4,22 @@ import { Link } from 'react-router-dom';
 class CalendarOverview extends Component {
   render() {
     return (
-      <div>
-        Calendar Overview
-        <Link to="/panel/calendars/add-new">Add New</Link>
+      <div className="box">
+        <div className="box-body">
+          <div className="form-group pull-right">
+            <Link className="btn btn-primary" to="/panel/calendars/add-new">Add New Calendar</Link>
+          </div>
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Calendar Name</th>
+                <th>Date Range</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            
+          </table>
+        </div>
       </div>
     );
   }
