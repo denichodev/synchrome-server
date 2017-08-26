d1a6b8cb41a24bc2c7e2fd7ee1687d6554863b02
Deni Cho <dencho.12@gmail.com>
2017-08-27 00:09:30 +0700

    Removed dotenv and added fastclick in package.json

--- public/mix.js
+++ public/mix.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 438);
+/******/ 	return __webpack_require__(__webpack_require__.s = 445);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -82,7 +82,7 @@
 
 /***/ }),
 
-/***/ 438:
+/***/ 445:
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(250);
