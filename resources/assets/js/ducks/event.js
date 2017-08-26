e31576c2d64929400f66b86718b111a051f5138b
Deni Cho <dencho.12@gmail.com>
2017-08-27 00:21:33 +0700

    Added event ducks

---
+++ resources/assets/js/ducks/event.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+// Types
+const FETCH_EVENT_CATEGORY_REQUEST = 'synchrome/event/fetch_event_category_request';
+const FETCH_EVENT_CATEGORY_SUCCESS = 'synchrome/event/fetch_event_category_success';
+const FETCH_EVENT_CATEGORY_FAILURE = 'synchrome/event/fetch_event_category_failure';
+
+export const eventTypes = {
+  FETCH_EVENT_CATEGORY_REQUEST,
+  FETCH_EVENT_CATEGORY_SUCCESS,
+  FETCH_EVENT_CATEGORY_FAILURE
+};
+
+// Action Creators
+const fetchEventCategoryRequest = () => {
+  return {
+    type: FETCH_EVENT_CATEGORY_REQUEST
+  };
+};
+
+const fetchEventCategorySuccess = payload => {
+  return {
+    type: FETCH_EVENT_CATEGORY_REQUEST,
+    payload
+  };
+};
+
+const fetchEventCategoryFailrue = payload => {
+  return {
+    type: FETCH_EVENT_CATEGORY_FAILURE,
+    payload
+  }
+}
+
+const fetchEventCategory = () => {
+  // TODO: implementing fetch event category
+}
+
+export const eventActions = {
+  fetchEventCategory
+}
+
+// Reducer
+const eventReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_EVENT_CATEGORY_SUCCESS:
+      return action.payload;
+    case FETCH_EVENT_CATEGORY_FAILURE:
+      return action.payload;
+    default:
+      return state;  
+  }
+}
+
+export default eventReducer;
