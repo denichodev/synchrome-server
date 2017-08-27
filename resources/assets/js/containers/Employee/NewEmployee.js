04b61122d70f77d9be94d04e8b68c25bb356de36
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-08-28 01:28:41 +0700

    Render fields, fetch echelons by agency on NewEmployee.js

--- resources/assets/js/containers/Employee/NewEmployee.js
+++ resources/assets/js/containers/Employee/NewEmployee.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import { agencyActions } from '../../ducks/agency';
+import { echelonActions } from '../../ducks/echelon';
 
 class NewEmployee extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      agency: null,
+      echelon: null
+    };
+  };
+
+  componentDidMount = () => {
+    const { fetchAllAgency } = this.props;
+
+    fetchAllAgency();
+  };
+
   renderTextField = field => {
     const { meta: { touched, error } } = field;
     const className = `form-group ${touched && error ? 'has-danger' : ''}`;
-
     const { label, name, input, placeholder } = field;
+
     return (
-      <div className={className}>
+      <div className={className}> 
         <label htmlFor={name}>{label}</label>
         <input
           type="text"
@@ -18,10 +35,81 @@ class NewEmployee extends Component {
           placeholder={placeholder}
           {...input}
         />
-        { error }
       </div>
     );
   };
+
+  renderAgenciesDropdown = (field) => {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+    const { label, name, input } = field;
+    const { agency, onChange } = this.props;
+
+    return (
+      <div className={className}>
+        <label htmlFor={name}>{label}</label>
+        <select 
+          className="form-control"
+          onChange
+          {...input}
+        >
+          {agency.data.map((row) => {
+            return (
+              <option key={row.id} value={row.id}>
+                {row.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    );
+  };
+
+  renderEchelonsDropdown = (field) => {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+    const { label, name, input } = field;
+    const { echelon } = this.props;
+
+    if (echelon != null) {
+      return (
+        <div className={className}>
+          <label htmlFor={name}>{label}</label>
+          <select 
+            className="form-control"
+            {...input}
+          >
+            {echelon.data.map((row) => {
+              return (
+                <option key={row.id} value={row.id}>
+                  {row.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      );
+    }
+    
+    return (
+      <div className={className}>
+        <label htmlFor={name}>{label}</label>
+        <select 
+          className="form-control"
+          {...input}
+        >
+        </select>
+      </div>
+    );
+  };
+
+  handleAgencyChange = (e) => {
+    console.log('Boom');
+
+    const { fetchEchelonsById } = this.props;
+
+    fetchEchelonsById(e.target.value);
+  }
 
   onSubmit = values => {
     console.log('VALUES', values);
@@ -31,17 +119,52 @@ class NewEmployee extends Component {
     const { handleSubmit } = this.props;
 
     return (
-      <div>
-        New Employee
-        <form onSubmit={handleSubmit(this.onSubmit)}>
-          <Field
-            label="Name"
-            name="testName"
-            placeholder="Enter Name"
-            component={this.renderTextField}
-          />
-          <button type="submit">Submit</button>
-        </form>
+      <div className="box">
+        <div className="box-header with-border">
+          <h3 className="box-title">Add New Employee</h3>
+        </div>
+        <div className="box-body">
+          <form onSubmit={handleSubmit(this.onSubmit)}>
+            <div className="row">
+              <div className="col-md-3">
+                <Field
+                  label="ID"
+                  name="id"
+                  component={this.renderTextField}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <Field
+                  label="Name"
+                  name="name"
+                  component={this.renderTextField}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-3">
+                <Field
+                  label="Agency"
+                  name="agency_id"
+                  component={this.renderAgenciesDropdown}
+                  onChange={this.handleAgencyChange}
+                />
+              </div>
+              <div className="col-md-3">
+                <Field
+                  label="Echelon"
+                  name="echelon_id"
+                  component={this.renderEchelonsDropdown}
+                />
+              </div>
+            </div>
+            <div class="form-group">
+              <button type="submit" className="btn btn-primary">Save</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
@@ -50,8 +173,8 @@ class NewEmployee extends Component {
 const validate = values => {
   const errors = {};
 
-  if (!values.testName) {
-    errors.testName = 'Nama tidak boleh kosong'
+  if (!values.name) {
+    errors.name = 'Nama tidak boleh kosong'
   }
 
   return errors;
@@ -62,4 +185,16 @@ const formOptions = {
   validate
 };
 
-export default reduxForm(formOptions)(NewEmployee);
+const mapStateToProps = (state) => ({
+  agency: state.agency,
+  echelon: state.echelon
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchAllAgency: () => dispatch(agencyActions.fetchAllAgency()),
+  fetchEchelonsById: (id) => dispatch(echelonActions.fetchEchelonsById(id))
+});
+
+const Connector = connect(mapStateToProps, mapDispatchToProps)(NewEmployee);
+
+export default reduxForm(formOptions)(Connector);
