5dc8c0699cfe82a65184a789675ea415f2f4c521
Deni Cho <dencho.12@gmail.com>
2017-08-29 10:56:45 +0700

    Added form style to error rendering

--- resources/assets/js/components/Forms/FormDatePicker.js
+++ resources/assets/js/components/Forms/FormDatePicker.js
@@ -3,18 +3,32 @@ import DatePicker from 'react-bootstrap-date-picker';
 
 const FormDatePicker = field => {
   const { meta: { pristine, error } } = field;
-  // const className = `form-group ${!pristine && error ? 'has-error' : ''}`;
+  const className = `form-group ${!pristine && error ? 'has-error' : ''}`;
   const { input, label, name, minDate, maxDate, calendarId } = field;
+  const errorHelpBlockStyle = {
+    visibility: `${!pristine && error ? 'visible' : 'hidden'}`
+  };
 
   return (
-    <DatePicker
-      id={calendarId}
-      value={input.value}
-      onChange={input.onChange}
-      className={`form-control`}
-      minDate={minDate ? minDate : null}
-      maxDate={maxDate ? maxDate : null}
-    />
+    <div className={className}>
+      <label htmlFor={name}>
+        {label}
+      </label>
+      <div className="input-group">
+        <div className="input-group-addon">
+          <i className="fa fa-calendar" />
+        </div>
+        <DatePicker
+          id={calendarId}
+          value={input.value}
+          onChange={input.onChange}
+          className={`form-control`}
+          minDate={minDate ? minDate : null}
+          maxDate={maxDate ? maxDate : null}
+        />
+      </div>
+      <span className="help-block" style={errorHelpBlockStyle}>{error}</span>
+    </div>
   );
 };
 
--- resources/assets/js/components/Forms/FormText.js
+++ resources/assets/js/components/Forms/FormText.js
@@ -5,23 +5,34 @@ const FormText = field => {
   const { meta: { touched, error } } = field;
   const className = `form-group ${touched && error ? 'has-error' : ''}`;
   const { label, name, input, placeholder } = field;
+  const errorHelpBlockStyle = {
+    visibility: `${touched && error ? 'visible' : 'hidden'}`
+  };
 
   return (
-    <div className={className}> 
-      <label htmlFor={name}>{label}</label>
+    <div className={className}>
+      <label htmlFor={name}>
+        {label}
+      </label>
       <input
         type="text"
         className="form-control"
         placeholder={placeholder}
         {...input}
       />
+      <span className="help-block" style={errorHelpBlockStyle}>{error}</span>
     </div>
   );
 };
 
+FormText.defaultProps = {
+  placeholder: '',
+  label: ''
+};
+
 FormText.propTypes = {
   label: PropTypes.string,
-  placeHolder: PropTypes.string
+  placeholder: PropTypes.string
 };
 
 export default FormText;
