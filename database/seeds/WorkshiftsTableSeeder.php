12f6c213e6ad9554e4c9d6dc26867755709128ce
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-09-06 01:06:08 +0700

    Fixed typo on WorkshiftsTableSeeder

--- database/seeds/WorkshiftsTableSeeder.php
+++ database/seeds/WorkshiftsTableSeeder.php
@@ -13,6 +13,7 @@ class WorkshiftsTableSeeder extends Seeder
     public function run()
     {
         Workshift::create([
+            'id' => 1,
             'name' => 'Normal Workday',
             'in' => '08:00:00',
             'out' => '17:00:00'
