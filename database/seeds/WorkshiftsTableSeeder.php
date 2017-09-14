82138335124eefb2c05d38c6ff5d9d179193cc9d
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-09-14 10:23:04 +0700

    Updated WorkshiftsTableSeeder

--- database/seeds/WorkshiftsTableSeeder.php
+++ database/seeds/WorkshiftsTableSeeder.php
@@ -2,9 +2,49 @@
 
 use Illuminate\Database\Seeder;
 use App\Workshift;
+use App\Shift;
 
 class WorkshiftsTableSeeder extends Seeder
 {
+    protected $workshifts = [
+        [
+            'id' => 1,
+            'name' => 'Guru Sekolah',
+            'details' => [
+                [
+                    'day' => 'Mon',
+                    'in' => '07:00',
+                    'out' => '14:00'
+                ],
+                [
+                    'day' => 'Tue',
+                    'in' => '07:00',
+                    'out' => '14:00'
+                ],
+                [
+                    'day' => 'Wed',
+                    'in' => '07:00',
+                    'out' => '14:00'
+                ],
+                [
+                    'day' => 'Thu',
+                    'in' => '07:00',
+                    'out' => '14:00'
+                ],
+                [
+                    'day' => 'Fri',
+                    'in' => '07:00',
+                    'out' => '14:00'
+                ],
+                [
+                    'day' => 'Sat',
+                    'in' => '07:00',
+                    'out' => '14:00'
+                ],
+            ]
+        ]
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -12,11 +52,18 @@ class WorkshiftsTableSeeder extends Seeder
      */
     public function run()
     {
-        Workshift::create([
-            'id' => 1,
-            'name' => 'Normal Workday',
-            'in' => '08:00:00',
-            'out' => '17:00:00'
-        ]);
+        foreach ($this->workshifts as $data) {
+            $workshift = Workshift::create([
+                'id' => $data['id'],
+                'name' => $data['name']
+            ]);
+            $details = [];
+
+            foreach ($data['details'] as $detail) {
+                array_push($details, new Shift($detail));
+            }
+
+            $workshift->details()->saveMany($details);
+        }
     }
 }
