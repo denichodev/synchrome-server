bc9c8df2fe5f16a6b6c42a9f71a395ed4d674867
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-09-14 11:00:49 +0700

    Call ReligionsTableSeeder on DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(EchelonsTableSeeder::class);
         $this->call(WorkshiftsTableSeeder::class);
         $this->call(AbsenceTypesTableSeeder::class);
+        $this->call(ReligionsTableSeeder::class);
     }
 }
