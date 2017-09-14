439678447d260f7cfa9c99bc03f9d66e23b47690
Adhyaksa Jaya <adhyaksaj@gmail.com>
2017-09-14 13:10:39 +0700

    Added SoftDeletes capability to Workshift and Shift

--- app/Shift.php
+++ app/Shift.php
@@ -3,13 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Shift extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'workshift_id',
         'day',
         'in',
         'out'
+    ];
+
+    protected $dates = [
+        'deleted_at'
     ];
 }
--- app/Workshift.php
+++ app/Workshift.php
@@ -3,12 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Workshift extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'id',
         'name'
+    ];
+
+    protected $dates = [
+        'deleted_at'
     ];
 
     public function details()
