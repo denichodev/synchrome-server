d320771e69a2a94ea1d906b028ab81480d78976b
Deni Cho <dencho.12@gmail.com>
2017-09-17 23:26:15 +0700

    Added react-table dependency and implemented the basic features of it

--- resources/assets/js/containers/Employee/EmployeeOverview.js
+++ resources/assets/js/containers/Employee/EmployeeOverview.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import matchSorter from 'match-sorter';
 import { employeeActions } from '../../ducks/employee';
+import FilterableTable from '../../components/DataTable/FilterableTable';
 
 class EmployeeOverview extends Component {
   componentDidMount() {
@@ -16,33 +18,58 @@ class EmployeeOverview extends Component {
     if (employeeData.length <= 0) {
       return (
         <tr>
-          <td colSpan="5"><center>No employee added yet</center></td>
+          <td colSpan="5">
+            <center>No employee added yet</center>
+          </td>
         </tr>
       );
     }
 
-    return (
-      employeeData.map((emp) => (
-        <tr key={emp.id}>
-          <td>{emp.id}</td>
-          <td>{emp.name}</td>
-          <td>{emp.echelon.agency.name}</td>
-          <td>{emp.echelon.name}</td>
-          <td>
-            <Link to={`/panel/employees/${emp.id}`} className="btn btn-primary btn-xs">Edit</Link>&nbsp;
-            <button onClick={() => this.handleDeleteClick(emp.id)} type="button" className="btn btn-danger btn-xs">Delete</button>
-          </td>
-        </tr>
-      ))
-    );
+    return employeeData.map(emp => (
+      <tr key={emp.id}>
+        <td>{emp.id}</td>
+        <td>{emp.name}</td>
+        <td>{emp.echelon.agency.name}</td>
+        <td>{emp.echelon.name}</td>
+        <td>
+          <Link
+            to={`/panel/employees/${emp.id}`}
+            className="btn btn-primary btn-xs"
+          >
+            Edit
+          </Link>&nbsp;
+          <button
+            onClick={() => this.handleDeleteClick(emp.id)}
+            type="button"
+            className="btn btn-danger btn-xs"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    ));
+  }
+
+  getEmployeeData = () => {
+    const { employeeData } = this.props;
+
+    return employeeData.map(emp => {
+      return {
+        id: emp.id,
+        name: emp.name,
+        agencyName: emp.echelon.agency.name,
+        echelonName: emp.echelon.name
+      };
+    });
   }
 
   handleDeleteClick = id => {
     const { deleteEmployee } = this.props;
     deleteEmployee(id);
-  }
+  };
 
   render() {
+    console.log('employee data', this.getEmployeeData());
     return (
       <div className="box">
         <div className="box-header">
@@ -50,9 +77,11 @@ class EmployeeOverview extends Component {
         </div>
         <div className="box-body">
           <div className="form-group pull-right">
-            <Link className="btn btn-primary" to="/panel/employees/add-new">Add New Employee</Link>
+            <Link className="btn btn-primary" to="/panel/employees/add-new">
+              Add New Employee
+            </Link>
           </div>
-          <table className="table table-striped table-bordered">
+          {/* <table className="table table-striped table-bordered">
             <thead>
               <tr>
                 <th>NIP</th>
@@ -65,21 +94,57 @@ class EmployeeOverview extends Component {
             <tbody>
               {this.renderEmployeeTable()}
             </tbody>
-          </table>
+          </table> */}
+          <div className="col-md-12">
+            <FilterableTable
+              data={mockData}
+              columns={columns}
+            />
+          </div>
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
+const columns = [
+  {
+    Header: 'NIP',
+    accessor: 'id'
+  },
+  {
+    Header: 'Name',
+    accessor: 'name',
+    filterMethod: (filter, rows) => matchSorter(rows, filter.value, { keys: ['name'] }),
+    filterAll: true
+  },
+  {
+    Header: 'Agency',
+    accessor: 'agency'
+  },
+  {
+    Header: 'Echelon',
+    accessor: 'echelon'
+  },
+];
+
+const mockData = [
+  {
+    id: '123124',
+    name: 'Deni Cho',
+    agency: 'Hello Agency',
+    echelon: 'Hello Echelon'
+  }
+];
+
+const mapStateToProps = state => ({
   employeeData: state.employee.data,
   employeeError: state.employee.error
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   fetchAllEmployee: () => dispatch(employeeActions.fetchAllEmployee()),
-  fetchEmployeeById: (id) => dispatch(employeeActions.fetchEmployeeById(id)),
+  fetchEmployeeById: id => dispatch(employeeActions.fetchEmployeeById(id)),
   deleteEmployee: id => dispatch(employeeActions.deleteEmployee(id))
 });
 
