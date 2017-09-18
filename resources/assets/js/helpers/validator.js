613f26687334a1939ae6828c099d413a4e444bdc
Deni Cho <dencho.12@gmail.com>
2017-09-18 22:10:48 +0700

    Updated validator file and added validator for NewUser

--- resources/assets/js/helpers/validator.js
+++ resources/assets/js/helpers/validator.js
@@ -1,5 +1,10 @@
 const required = value => (value ? undefined : 'Required');
+
+const maxLength = max => value => (value && value.length > max ? `Must be ${max} characters or less` : undefined);
+const minLength = min => value => (value && value.length < min ? `Must be ${min} characters or more` : undefined);
+
 const number = value => (value && isNaN(Number(value)) ? 'Must be a number' : undefined);
+
 const email = value => (
   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
     ? 'Invalid email address'
@@ -8,5 +13,7 @@ const email = value => (
 export default {
   required,
   number,
-  email
+  email,
+  maxLength,
+  minLength
 };
